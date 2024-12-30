c830bea29a0899fd3e3c5f975de48b9187f93e84
akmalmakhmudov <akmalmakhmudov@ADMINui-MacBookPro-2.local>
2024-12-31 07:18:30 +0900

    Change API key

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,12 +6,12 @@ import { FIREBASE_API_KEY } from '@env';
 
 const firebaseConfigProduction = {
   apiKey: FIREBASE_API_KEY,
-  authDomain: "bebrand-83e0a.firebaseapp.com",
-  projectId: "bebrand-83e0a",
-  storageBucket: "bebrand-83e0a.firebasestorage.app",
-  messagingSenderId: "428697527024",
-  appId: "1:428697527024:web:1f987f3a9b518956809835",
-  measurementId: "G-PXFD7GVP78"
+  authDomain: "bebrand-66c17.firebaseapp.com",
+  projectId: "bebrand-66c17",
+  storageBucket: "bebrand-66c17.firebasestorage.app",
+  messagingSenderId: "73663823346",
+  appId: "1:73663823346:web:346b9fab72b165f4143f6b",
+  measurementId: "G-SZ7TZDLL0E"
 };
 
 if (!firebase.apps.length) {
