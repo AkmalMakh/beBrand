4691626253e95f04fa74c45e9e44189cd6f0d73f
akmalmakhmudov <akmalmakhmudov@ADMINui-MacBookPro-2.local>
2024-12-31 06:57:35 +0900

    Remove sensitive information

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,19 +2,10 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyACuJ7ZUZBgz4-wYz1iOISVwzJ1r5hJ_fM',
-  authDomain: 'bebrand-c026e.firebaseapp.com',
-  projectId: 'bebrand-c026e',
-  storageBucket: 'bebrand-c026e.appspot.com',
-  messagingSenderId: '454787406778',
-  appId: '1:454787406778:web:7b50cc2f2c4bf8c87f9b83',
-  measurementId: 'G-4FSL31R0SR',
-};
+import { FIREBASE_API_KEY } from '@env';
 
 const firebaseConfigProduction = {
-  apiKey: "AIzaSyDk6BOJrpSMH-nzQALW1yN87xPf3GdP51M",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "bebrand-83e0a.firebaseapp.com",
   projectId: "bebrand-83e0a",
   storageBucket: "bebrand-83e0a.firebasestorage.app",
